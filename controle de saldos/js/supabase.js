c6b88fefb546d6ab49cfc38ff29c0582afef7c3a
municipiopitangueiraspr-arch <municipiopitangueiras.pr@gmail.com>
2026-09-22 15:54:13 -0300

    Delete controle de saldos directory

--- controle de saldos/js/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-
-const SUPABASE_URL = "https://qgkjnzcqjhhqdgxmvtew.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_gbXPIpkbYvf3YKITplkjpg_eKrPHhYw";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
